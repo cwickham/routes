a628702563666e1aae8149c0ecd486ff55987445
Charlotte Wickham <cwickham@gmail.com>
2019-07-23 15:50:24 -0700

    add docs, remove routes/ folder from paths

--- R/setup.R
+++ R/setup.R
@@ -1,57 +1,75 @@
+#' Setup directory and file for generating a route
+#' 
+#' The first step in creating a new route landscape.  
+#' @param from address, or latitude and longitude of the start of the route.
+#' @param to address, or latitude and longitude of the end of the route.
+#' @param shortname string suitable for use as directory name and in file names.
+#' @return Called for it's side effects only. Creates a directory called 
+#'  `shortname`, creates a file in this directory
+#'  called `00-setup.R` populated with a template.  
 #' @export
 setup <- function(from, to, shortname = paste0(from, "_", to)){
-  path <- fs::path("routes", shortname)
+  path <- fs::path(shortname)
   usethis::use_directory(path)
   
   usethis::use_template(
     template = "setup.R",
     save_as = fs::path(path, "00-setup.R"),
     data = list(to = to, from = from, shortname = shortname),
-    package = "routes2019",
+    package = "routes",
     open = TRUE)
 }
 
 #' Generate template for getting coordinates for route
 #'
+#' @param from address, or latitude and longitude of the start of the route.
+#' @param to address, or latitude and longitude of the end of the route.
+#' @param shortname string suitable for use as directory name and in file names.
+#'
 #' @export
 route <- function(from, to, shortname = paste0(from, "_", to)){
-  path <- fs::path("routes", shortname)
+  path <- fs::path(shortname)
   
   usethis::use_template(
     template = "route.Rmd",
     save_as = fs::path(path, "01-route.Rmd"),
     data = list(to = to, from = from, shortname = shortname),
-    package = "routes2019",
+    package = "routes",
     open = TRUE)
 }
 
 
 #' Generate template for getting streetview images
 #'
+#' @param from address, or latitude and longitude of the start of the route.
+#' @param to address, or latitude and longitude of the end of the route.
+#' @param shortname string suitable for use as directory name and in file names.
 #' @export
 streetview <- function(from, to, shortname = paste0(from, "_", to)){
-  path <- fs::path("routes", shortname)
+  path <- fs::path(shortname)
   usethis::use_directory(fs::path(path, "images"))
   
   usethis::use_template(
     template = "streetview.Rmd",
     save_as = fs::path(path, "02-streetview.Rmd"),
     data = list(to = to, from = from, shortname = shortname),
-    package = "routes2019",
+    package = "routes",
     open = TRUE)
 }
 
-#' Generate template for clsutering, counting and plotting colors
-#'
+#' Generate template for clustering, counting and plotting colors
+#' @param from address, or latitude and longitude of the start of the route.
+#' @param to address, or latitude and longitude of the end of the route.
+#' @param shortname string suitable for use as directory name and in file names.
 #' @export
 cluster <- function(from, to, shortname = paste0(from, "_", to)){
-  path <- fs::path("routes", shortname)
+  path <- fs::path(shortname)
   
   usethis::use_template(
     template = "cluster.Rmd",
     save_as = fs::path(path, "03-cluster.Rmd"),
     data = list(to = to, from = from, shortname = shortname),
-    package = "routes2019",
+    package = "routes",
     open = TRUE)
 }
 
--- inst/templates/setup.R
+++ inst/templates/setup.R
@@ -1,14 +1,14 @@
 # Building a route landscape from {{{ from }}} to {{{ to }}}
 
 # Step 1: Get the route coordinates
-routes2019::route(from = "{{{ from }}}",
+routes::route(from = "{{{ from }}}",
   to = "{{{ to }}}", shortname = "{{{ shortname }}}")
 
 # Step 2: Get the StreetView images
-routes2019::streetview(from = "{{{ from }}}",
+routes::streetview(from = "{{{ from }}}",
   to = "{{{ to }}}", shortname = "{{{ shortname }}}")
 
 # Step 3: Cluster, count and plot the colors
-routes2019::cluster(from = "{{{ from }}}",
+routes::cluster(from = "{{{ from }}}",
   to = "{{{ to }}}", shortname = "{{{ shortname }}}")
 
